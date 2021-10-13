8f628c4366481d622475f6e851ab28a122029e94
prolific-dev <de391ago@htwg-konstanz.de>
2021-10-13 15:01:55 +0200

    Adding ability for Player and Troop to be in a Team.

--- src/main/scala/model/LegalField.scala
+++ src/main/scala/model/LegalField.scala
@@ -3,5 +3,7 @@ package de.htwg.se.riskgame.model
 case class LegalField(name: String, troop: Troop) extends Field {
   def this(name: String) = this(name, new Troop(1))
 
+  def team(): Team = troop.team
+
   override def toString: String = troop.toString
 }
--- src/test/scala/model/LegalFieldSpec.scala
+++ src/test/scala/model/LegalFieldSpec.scala
@@ -7,6 +7,9 @@ class LegalFieldSpec extends AnyWordSpec with Matchers {
   "A LegalField" when {
     "new" should {
       val field = new LegalField("LegalField", new Troop(1))
+      "have no team" in {
+        field.team() should be(NO_TEAM)
+      }
       "have a nice String representation" in {
         field.toString() should be("1")
       }
