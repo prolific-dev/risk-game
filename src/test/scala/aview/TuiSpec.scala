6efd7392991bc2bac26a646c3472351c4edee515
prolific-dev <de391ago@htwg-konstanz.de>
2021-10-21 01:45:29 +0200

    Improve structure and tests.

--- src/test/scala/aview/TuiSpec.scala
+++ src/test/scala/aview/TuiSpec.scala
@@ -1,7 +1,7 @@
 package de.htwg.se.riskgame.aview
 
 import de.htwg.se.riskgame.controller.Controller
-import de.htwg.se.riskgame.model.Desk
+import de.htwg.se.riskgame.model.{Desk, Field, Team, Troop}
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
@@ -12,10 +12,23 @@ class TuiSpec extends AnyWordSpec with Matchers {
     "create an empty Risk Game on input 'n'" in {
       tui.processInputLine("n")
       controller.desk should be(new Desk(3))
+      controller.desk.size should be(3)
     }
     "create a random Risk Game on input 'r'" in {
       tui.processInputLine("r")
-      // test
+      controller.desk.valid() should be(true)
+    }
+    "set a field on input 's'" in {
+      tui.processInputLine("s")
+      controller.desk.field(0, 0) should be(Field("Field", Troop(3, Team.BLUE)))
+    }
+    "set colorization on with input 'c on'" in {
+      tui.processInputLine("c on")
+      controller.desk.isColorized should be(true)
+    }
+    "set colorization off with input 'c off'" in {
+      tui.processInputLine("c off")
+      controller.desk.isColorized should be(false)
     }
   }
 }
