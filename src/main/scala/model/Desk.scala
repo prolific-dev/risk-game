57602c37d29bfc4cb83d2e3090485486cb2b5ea4
prolific-dev <de391ago@htwg-konstanz.de>
2021-10-21 13:13:42 +0200

    Improve structure and tests.

--- src/main/scala/model/Desk.scala
+++ src/main/scala/model/Desk.scala
@@ -5,12 +5,34 @@ import scala.io.AnsiColor.*
 case class Desk(fields: Matrix[IField], isColorized: Boolean) {
   def this(fields: Matrix[IField]) = this(fields, false)
 
-  def this(size: Int) = this(new Matrix[IField](size, new Field("Free IField")))
+  def this(size: Int) = this(new Matrix[IField](size, new Field()))
 
   val size: Int = fields.size
 
   def set(row: Int, col: Int, value: IField): Desk = copy(fields.replaceField(row, col, value))
 
+  def setColorizedOn(): Desk = copy(fields, isColorized = true)
+
+  def setColorizedOff(): Desk = copy(fields, isColorized = false)
+
+  def field(row: Int, col: Int): IField = fields.field(row, col)
+
+  def neighbors(i: Int, j: Int): Neighbors = new Neighbors(i, j, fields)
+
+  def valid(): Boolean = {
+    var valid = true
+    var i = 0
+    while (valid && i < size) {
+      for (j <- 0 until size) {
+        if (!neighbors(i, j).valid()) {
+          valid = false
+        }
+      }
+      i += 1
+    }
+    valid
+  }
+  
   override def toString: String = {
 
     def consoleFieldString(field: IField): String = {
@@ -37,26 +59,4 @@ case class Desk(fields: Matrix[IField], isColorized: Boolean) {
     sb.append(TOP_BOTTOM_LINE)
     sb.toString()
   }
-
-  def setColorizedOn(): Desk = copy(fields, isColorized = true)
-
-  def setColorizedOff(): Desk = copy(fields, isColorized = false)
-
-  def field(row: Int, col: Int): IField = fields.field(row, col)
-
-  def valid(): Boolean = {
-    var valid = true
-    var i = 0
-    while (valid && i < size) {
-      for (j <- 0 until size) {
-        if (!neighbors(i, j).valid()) {
-          valid = false
-        }
-      }
-      i += 1
-    }
-    valid
-  }
-
-  def neighbors(i: Int, j: Int): Neighbors = new Neighbors(i, j, fields)
 }
--- src/main/scala/model/Field.scala
+++ src/main/scala/model/Field.scala
@@ -3,7 +3,7 @@ package de.htwg.se.riskgame.model
 case class Field(name: String, troop: Troop) extends IField {
   def this(name: String) = this(name, new Troop(1))
 
-  def this() = this("Free IField")
+  def this() = this("Free Field")
 
   override def getTroop(): Option[Troop] = Some(troop)
 
