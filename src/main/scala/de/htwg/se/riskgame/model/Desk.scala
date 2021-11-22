8091f5389771a322b7d3934cf1c6fc4833415e79
prolific-dev <de391ago@htwg-konstanz.de>
2021-11-22 22:37:50 +0100

    Raising functionality of Desk methods.

--- src/main/scala/de/htwg/se/riskgame/model/Desk.scala
+++ src/main/scala/de/htwg/se/riskgame/model/Desk.scala
@@ -12,35 +12,25 @@ case class Desk(fields: Matrix[Field]) {
   def neighbors(i: Int, j: Int): Neighbors = new Neighbors(i, j, fields)
 
   override def toString: String = {
-    val sb = new StringBuilder("\n")
-    val TOP_BOTTOM_LINE = ("+-" + ("-----" * size)) + "-+\n"
-    val LEFT_WALL = "| "
-    val RIGHT_WALL = " |\n"
-
     def consoleFieldString(field: Field): String = {
       field.team() match
         case Team.NO_TEAM => field.toString
         case _ => field.team().getAnsi + field.toString + RESET
     }
 
-    sb ++= TOP_BOTTOM_LINE
-    for (i <- 0 until size) {
-      sb ++= LEFT_WALL
-      for (j <- 0 until size) {
-        sb ++= "  " + consoleFieldString(field(i, j)) + "  "
-      }
-      sb ++= RIGHT_WALL
-    }
-    sb ++= TOP_BOTTOM_LINE
+    val sb = new StringBuilder(("\n+-" + ("-----" * size)) + "-+\n")
+    (0 until size).foreach(i => {
+      sb ++= "| "; (0 until size).foreach(j => sb ++= "  " + consoleFieldString(field(i, j)) + "  "); sb ++= " |\n"
+    })
+    sb ++= ("+-" + ("-----" * size)) + "-+\n"
     sb.toString()
   }
 
   def valid(): Boolean = {
     var valid = true
-    for {
-      i <- 0 until size
-      j <- 0 until size; if (!neighbors(i, j).valid())
-    } valid = false
+    (0 until size).foreach(i => (0 until size).foreach(j => if (!neighbors(i, j).valid()) {
+      valid = false
+    }))
     valid
   }
 
