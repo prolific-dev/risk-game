81db21f31435f440f72892ad5aac6778610df454
prolific-dev <de391ago@htwg-konstanz.de>
2021-11-26 20:03:24 +0100

    Merging BlockedFieldSpec and OccupiedFieldSpec tests under one FieldSpec.

--- src/test/scala/de/htwg/se/riskgame/model/FieldSpec.scala
+++ src/test/scala/de/htwg/se/riskgame/model/FieldSpec.scala
@@ -1,5 +1,60 @@
 package de.htwg.se.riskgame.model
 
-class FieldSpec {
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class FieldSpec extends AnyWordSpec with Matchers {
+  "A field" when {
+    "called as an object with the factory method pattern" should {
+      val blockedField = Field("x")
+      val occupiedField = Field("free")
+      val definedOccupiedField = Field("Defined Field", new Troop(3))
+      "be instance of" in {
+        blockedField.isInstanceOf[BlockedField] should be(true)
+        occupiedField.isInstanceOf[OccupiedField] should be(true)
+        definedOccupiedField.isInstanceOf[OccupiedField] should be(true)
+      }
+    }
+    "call directly by apply method" should {
+      val blockedField = Field.apply("x")
+      val occupiedField = Field.apply("free")
+      val definedOccupiedField = Field.apply("Defined Field", new Troop(3))
+      "be instance of" in {
+        blockedField.isInstanceOf[BlockedField] should be(true)
+        occupiedField.isInstanceOf[OccupiedField] should be(true)
+        definedOccupiedField.isInstanceOf[OccupiedField] should be(true)
+      }
+    }
+    "as OccupiedField" should {
+      val field = new OccupiedField("OccupiedField", new Troop(1))
+      "have default Troop set" in {
+        field.getTroop() should be(Some(new Troop(1)))
+      }
+      "have no team" in {
+        field.team() should be(Team.NO_TEAM)
+      }
+      "should not be set" in {
+        field.isSet() should be(false)
+      }
+      "have a nice String representation" in {
+        field.toString() should be("1")
+      }
+    }
+    "as BlockedField" should {
+      val field = new BlockedField()
+      "have no troops" in {
+        field.getTroop() should be(None)
+      }
+      "have no team" in {
+        field.team() should be(Team.NO_TEAM)
+      }
+      "should not be set" in {
+        field.isSet() should be(true)
+      }
+      "have a nice String representation" in {
+        field.toString should be("x")
+      }
+    }
+  }
 
 }
