3418bfa878211eb8917def7053b5023d169df55d
prolific-dev <de391ago@htwg-konstanz.de>
2021-11-26 19:46:05 +0100

    Minimal testing of main object class.

--- src/test/scala/de/htwg/se/riskgame/RiskGameSpec.scala
+++ src/test/scala/de/htwg/se/riskgame/RiskGameSpec.scala
@@ -1,6 +1,18 @@
 package de.htwg.se.riskgame
 
+import de.htwg.se.riskgame.aview.*
+import de.htwg.se.riskgame.controller.Controller
+import de.htwg.se.riskgame.model.*
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
-class RiskGameSpec extends AnyWordSpec with Matchers {}
+class RiskGameSpec extends AnyWordSpec with Matchers {
+  "A RiskGame" when {
+    "by default" should {
+      "have a controller and a tui" in {
+        RiskGame.controller.isInstanceOf[Controller] should be(true)
+        RiskGame.tui.isInstanceOf[Tui] should be(true)
+      }
+    }
+  }
+}
