5945d3a9b506b348185a8246383c2a30a9069718
prolific-dev <de391ago@htwg-konstanz.de>
2022-01-31 04:20:56 +0100

    Adding Closing GameStatus

--- src/main/scala/de/htwg/se/riskgame/controller/GameStatus.scala
+++ src/main/scala/de/htwg/se/riskgame/controller/GameStatus.scala
@@ -1,17 +1,18 @@
 package de.htwg.se.riskgame.controller
 
-enum GameStatus(status: String):
+enum GameStatus (status: String):
   def message: String = status
 
-  case IDLE extends GameStatus("")
-  case EMPTY extends GameStatus("A new empty game was created")
-  case NEW extends GameStatus("A new game was created")
-  case SET extends GameStatus("A field was set")
-  case SAVED extends GameStatus("Game saved")
-  case LOADED extends GameStatus("Game loaded")
-  case UNDO extends GameStatus("Undone one step")
-  case REDO extends GameStatus("Redone one step")
-  case WON extends GameStatus("Player won the game")
-  case LOST extends GameStatus("Player lost the game")
-end GameStatus
+  case IDLE extends GameStatus ("")
+  case EMPTY extends GameStatus ("A new empty game was created")
+  case NEW extends GameStatus ("A new game was created")
+  case SET extends GameStatus ("A field was set")
+  case SAVED extends GameStatus ("Game was saved")
+  case LOADED extends GameStatus ("Game was loaded")
+  case UNDO extends GameStatus ("Undone one step")
+  case REDO extends GameStatus ("Redone one step")
+  case WON extends GameStatus ("Player won the game")
+  case LOST extends GameStatus ("Player lost the game")
+  case CLOSING extends GameStatus ("Game is closing")
+  end GameStatus
 
