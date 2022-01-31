5973eb6c53b9d9db2da57828e530156c39f86040
prolific-dev <de391ago@htwg-konstanz.de>
2022-01-31 04:30:30 +0100

    Reformate Main

--- src/main/scala/de/htwg/se/riskgame/RiskGame.scala
+++ src/main/scala/de/htwg/se/riskgame/RiskGame.scala
@@ -2,22 +2,12 @@ package de.htwg.se.riskgame
 
 
 import com.google.inject.{Guice, Injector}
-import de.htwg.se.riskgame.RiskGame.controller
 import de.htwg.se.riskgame.aview.gui.GUI
 import de.htwg.se.riskgame.aview.tui.TUI
 import de.htwg.se.riskgame.controller.controllerComponent.ControllerInterface
 import de.htwg.se.riskgame.controller.controllerComponent.controllerBasicImpl.Controller
 import de.htwg.se.riskgame.model.deskComponent.DeskInterface
-import de.htwg.se.riskgame.model.deskComponent.deskBasicImpl.Desk
-import de.htwg.se.riskgame.model.deskComponent.deskBasicImpl.deskCreatorComponent.deskCreatorBasicImpl.DeskCreateContinentMapStrategy
 import scalafx.application.JFXApp3
-
-import scala.io.StdIn.readLine
-
-
-@main def run(): Unit =
-  println("Hello")
-  RiskGame.runTUI()
 
 
 object RiskGame {
